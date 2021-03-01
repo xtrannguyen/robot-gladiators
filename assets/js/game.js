b7b93c36c0cb5a29cd798dc43e6500968fac9836
xtrannguyen <tnguyenmw@gmail.com>
2021-03-01 17:33:21 -0500

    added console logs

---
+++ assets/js/game.js
@@ -0,0 +1,16 @@
+var playerName = window.prompt("What is your robot's name?");
+// what is this?
+console.log(playerName);
+console.log("This logs a string, good for leaving youself a message");
+//this will do math and log 20
+console.log(10+10);
+// what is this
+console.log("Our robot's name is " + playerName);
+
+
+// this creates a function named "fight"
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+// fight();
